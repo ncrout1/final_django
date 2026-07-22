6e686be6b5e7c8ec4b334ea6b1734b3cc9284538
ncrout1 <ncrout1999@gmail.com>
2026-07-22 14:01:13 +0530

    22_july

---
+++ narsingh/rout1.py
@@ -0,0 +1,19 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+print("Start small. Ship something.")
+
+
+def mazedar(fun):
+    def calling ():
+        fun()
+    return calling
+@mazedar
+def jindagi():
+    print("Hello Bhai")
+
+# hello=mazedar(jindagi)
+# hello()
+
+
+# mazedar(jindagi)
+jindagi()
